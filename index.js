7882aeff59c1a07a287c6abfa2fe38fd67e6443d
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-06-09 01:20:53 +0600

    cors included

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 const port = 5000;
 
+app.use(cors());
 
 app.get('/' , (req,res) => {
     res.send("Port is started successfully");
