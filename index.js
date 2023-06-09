b345b6588745de0bf29f792be100bc4a8a794390
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-06-09 11:15:55 +0600

    chefs data are displying in server

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const port = 5000;
 
 app.use(cors());
 
+const chefs_detail = require('./data/chefs_data.json');
+
 app.get('/' , (req,res) => {
     res.send("Port is started successfully");
+})
+
+app.get('/chefs' , (req,res) => {
+    res.send(chefs_detail);
 })
 
 app.listen(port , ()=>{
